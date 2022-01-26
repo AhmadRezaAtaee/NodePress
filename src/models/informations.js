f23dc5c39d190510fd5959ccd919689c0916d90c
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-01-26 20:10:42 +0330

    feat(models): add informations model

---
+++ src/models/informations.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const { model, Schema } = mongoose
+
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    value: String
+})
+
+export default model('Users', schema)
