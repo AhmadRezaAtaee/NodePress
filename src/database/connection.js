1a7ecb46a576c82710d4a7c4b8e85993b2d61dc3
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-01-27 19:03:42 +0330

    fix(database): fix database connection for returning connection instead of logging

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose"
 
 export default mongoose.connect(
-    process.env.DB_CONNECT,
-    error => error ? console.error("Database connection error: " + error) : console.log("Connected to database!")
+    process.env.DB_CONNECT
 )
