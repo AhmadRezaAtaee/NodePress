5b8e2a481f64a74197608c2cda8e584a26722ebd
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-01-28 15:07:58 +0330

    feat(models): add post-categories model

--- src/models/post-categories.js
+++ src/models/post-categories.js
@@ -1,15 +1,24 @@
-import mongoose from "mongoose";
-const { model, Schema } = mongoose
+import mongoose from 'mongoose';
+import Joi from 'joi';
+const { model, Schema } = mongoose;
 
 const schema = new Schema({
-    post_id: {
-        type: Number,
-        required: true
-    },
-    category_id: {
-        type: Number,
-        required: true
-    }
-})
+	post_id: {
+		type: String,
+		required: true,
+	},
+	category_id: {
+		type: String,
+		required: true,
+	},
+});
 
-export default model('Users', schema)
+export default model('Post-Categories', schema);
+
+const postCategoriesSchema = Joi.object({
+	post_id: Joi.string().required(),
+	category_id: Joi.string().required(),
+});
+
+export const postCategoriesValidation = (data) =>
+	postCategoriesSchema.validateAsync(data);
--- src/models/posts.js
+++ src/models/posts.js
@@ -1,29 +1,41 @@
-import mongoose from "mongoose";
-const { model, Schema } = mongoose
+import mongoose from 'mongoose';
+import Joi from 'joi';
+const { model, Schema } = mongoose;
 
 const schema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255
-    },
-    author_id: {
-        type: String,
-        required: true,
-        max: 500,
-        min: 6,
-    },
-    data: {
-        type: Date,
-        default: Date.now
-    },
-    post_status: Boolean,
-    comment_status: Boolean
-})
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+	},
+	author_id: {
+		type: String,
+		required: true,
+	},
+	data: {
+		type: Date,
+		default: Date.now,
+	},
+	post_status: {
+		type: Boolean,
+		default: true,
+	},
+	comment_status: {
+		type: Boolean,
+		default: true,
+	},
+});
 
-export default model('Users', schema)
+export default model('Posts', schema);
+
+const postsSchema = Joi.object({
+	title: Joi.string().required(),
+	content: Joi.string(),
+	author_id: Joi.string().required(),
+	post_status: Joi.boolean(),
+	comment_status: Joi.boolean(),
+});
+
+export const postsValidation = (data) => postsSchema.validateAsync(data);
