0776d070e65bdc22eec3fae1e81dcf35ab18bf2f
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-01-28 15:05:13 +0330

    feat(models): add comments model

--- src/models/comments.js
+++ src/models/comments.js
@@ -1,26 +1,34 @@
-import mongoose from "mongoose";
-const { model, Schema } = mongoose
+import mongoose from 'mongoose';
+import Joi from 'joi';
+const { model, Schema } = mongoose;
 
 const schema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    author_id: {
-        type: String,
-        required: true,
-    },
-    parent_id: {
-        type: Number
-    },
-    data: {
-        type: Date,
-        default: Date.now
-    }
-})
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: true,
+	},
+	author_id: {
+		type: String,
+		required: true,
+	},
+	parent_id: String,
+	data: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
-export default model('Users', schema)
+export default model('Comments', schema);
+
+const commentsSchema = Joi.object({
+	title: Joi.string().required(),
+	content: Joi.string().required(),
+	author_id: Joi.string().required(),
+	parent_id: Joi.string(),
+});
+
+export const commentsValidation = (data) => commentsSchema.validateAsync(data);
