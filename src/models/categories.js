cc51f5d9506a8a00ed7d24110eaa9ff3686cdd1b
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-01-28 15:05:13 +0330

    feat(models): add categories model

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,12 +1,21 @@
-import mongoose from "mongoose";
-const { model, Schema } = mongoose
+import mongoose from 'mongoose';
+import Joi from 'joi';
+const { model, Schema } = mongoose;
 
 const schema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    parent_id: Number
-})
+	name: {
+		type: String,
+		required: true,
+	},
+	parent_id: String,
+});
 
-export default model('Users', schema)
+export default model('Categories', schema);
+
+const categoriesSchema = Joi.object({
+	name: Joi.string().required(),
+	parent_id: Joi.string(),
+});
+
+export const categoriesValidation = (data) =>
+	categoriesSchema.validateAsync(data);
