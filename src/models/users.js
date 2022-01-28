27ebb8b4cc86d598df804ff23f3e271be080bd07
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-01-28 15:08:45 +0330

    feat(models): add users model

--- src/models/users.js
+++ src/models/users.js
@@ -1,35 +1,43 @@
-import mongoose from "mongoose";
-const { model, Schema } = mongoose
+import mongoose from 'mongoose';
+import Joi from 'joi';
+const { model, Schema } = mongoose;
 
 const schema = new Schema({
-    name: {
-        first: String,
-        last: String,
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 255
-    },
-    password: {
-        type: String,
-        required: true,
-        max: 500,
-        min: 6
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    data: {
-        type: Date,
-        default: Date.now
-    }
-})
+	name: String,
+	email: {
+		type: String,
+		required: true,
+		min: 6,
+		max: 255,
+	},
+	password: {
+		type: String,
+		required: true,
+		max: 500,
+		min: 6,
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+	data: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
-schema.virtual('fullName').get(() => this.name.first + " " + this.name.last)
+export default model('Users', schema);
 
-export default model('Users', schema)
+const registerSchema = Joi.object({
+	name: Joi.string().min(6),
+	email: Joi.string().min(6).required().email(),
+	password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+	email: Joi.string().min(6).required().email(),
+	password: Joi.string().min(6).required(),
+});
+
+export const registerValidation = (data) => registerSchema.validateAsync(data);
+export const loginValidation = (data) => loginSchema.validateAsync(data);
